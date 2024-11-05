853f336d9431a1a523b41f8cf4e6a4746863aa56
Ishu_official <somnathhor12052005@gmail.com>
2024-11-05 10:45:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_ID = int(getenv("API_ID", "24509589"))
-API_HASH = getenv("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75")
-
-BOT_TOKEN = getenv("BOT_TOKEN", "7732529579:AAFHBwOo1tsX5rqUToaRcCwgG_iilgRv2bE")
+API_HASH = getenv("API_HASH", ")
 OWNER_ID = int(getenv("OWNER_ID", "7070591202"))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Somu:Somu@somu.xbkiklu.mongodb.net/?retryWrites=true&w=majority&appName=Somu")
 MUST_JOIN = getenv("MUST_JOIN", "somueditingzone")
